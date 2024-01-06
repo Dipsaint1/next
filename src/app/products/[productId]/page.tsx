17c775559bf521a85083e868737f409f6a857a7b
Cyber9ja <ayodejioladapo96@gmail.com>
2024-01-06 10:44:02 +0000

    added not found page

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,4 +1,7 @@
-export default function ProductDetails({ params } : { params: { productId: string }}){
+// export default function ProductDetails({ params } : { params: { productId: string }}){
+export default function ProductDetails({ params : { productId }} : {
+  params: { productId: string }
+}){
   
-  return ( <h1>Details about product {params.productId}</h1> )
+  return ( <h1>Details about product { productId }</h1> )
 }
