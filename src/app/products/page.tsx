b39d0def246da0b4c71564bbf3428d360b48ac76
cyber9ja <ayodejioladapo15@gmail.com>
2024-05-14 16:03:50 +0100

    added navigation links

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 export default function ProductList(){
+  const productId = 100;
   return ( 
     <>
+      <Link href='/'>Home</Link>
       <h1>Product List</h1>
-      <h4>Product 1</h4>
-      <h4>Product 2</h4>
-      <h4>Product 3</h4>
+      <Link href='products/1'><h4>Product 1</h4></Link>
+      <Link href='products/2'><h4>Product 2</h4></Link>
+      <Link href='products/3' replace><h4>Product 3</h4></Link>
+      <Link href={`products/${productId}`}><h4>Product 4</h4></Link>
     </>
   )
 }
