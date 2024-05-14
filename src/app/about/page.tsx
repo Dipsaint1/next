e1378ed7c7a823a1412bcfa87baebe03815d0f89
cyber9ja <ayodejioladapo15@gmail.com>
2024-05-14 15:29:54 +0100

    added metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: 'About Cyber9ja'
+};
+
 export default function About() {
   return (
     <div>About</div>
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,28 @@
-// export default function ProductDetails({ params } : { params: { productId: string }}){
-export default function ProductDetails({ params : { productId }} : {
-  params: { productId: string }
-}){
-  
-  return ( <h1>Details about product { productId }</h1> )
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string
+  }
+}
+
+// export const generateMetadata = ({ params }: Props ): Metadata => {
+//   return {
+//     title: `Product ${params.productId}`
+//   }
+// };
+
+export const generateMetadata = async({ params }: Props ): Promise<Metadata> => {
+  const title = await new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`iphone ${params.productId}`)
+    }, 100);
+  })
+  return {
+    title: `Product ${title}`
+  }
+};
+
+export default function ProductDetails({params} : Props){
+  return ( <h1>Details about product { params.productId }</h1> )
 }
