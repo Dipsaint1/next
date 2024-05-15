d9d8668d2aa2b29b1c8bff4190ced862483ca2d6
cyber9ja <ayodejioladapo15@gmail.com>
2024-05-15 10:38:54 +0100

    added error page

--- src/app/products/[productId]/review/[reviewId]/page.tsx
+++ src/app/products/[productId]/review/[reviewId]/page.tsx
@@ -1,12 +1,18 @@
 import { notFound } from "next/navigation";
 // import NotFound from "./not-found";
 
+const getRandomInt = (count: number) => { 
+  return Math.floor(Math.random() * count);
+}
+
 export default function ReviewDetails({ params : { reviewId, productId } }: {
-  params: { 
-    reviewId: string,
-    productId: string
-  }
+  params: { reviewId: string, productId: string }
 }){
+  const randomNumber = getRandomInt(2);
+  if(randomNumber === 1){
+    throw new Error("Error Loading Review"); 
+  }
+
   if(parseInt(reviewId) > 1000){
     return notFound();
   }
