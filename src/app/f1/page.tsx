8ef596fcdfe1d9a014c6ecbd0704fa872ce4e452
cyber9ja <ayodejioladapo15@gmail.com>
2024-05-16 22:55:21 +0100

    minor update

--- src/app/f1/page.tsx
+++ src/app/f1/page.tsx
@@ -4,9 +4,7 @@ export default function F1() {
   return(
     <>
       <h1>F1 page</h1>
-      <div>
-        <Link href='/f1/f2'>F2</Link>
-      </div>
+      <Link href='/f1/f2'>F2</Link>
     </>
   )
 };
